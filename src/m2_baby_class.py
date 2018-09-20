4918985e276b1d3fe6f344c9e99e12e4ef5a759b
arock19 <guptaa5@rose-hulman.edu>
2018-09-20 16:09:13 -0400

    o

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Achintya Gupta.
+"""  # TO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -36,6 +36,30 @@ def main():
         mckinley.feed_baby()
         for j in range(4):
             mckinley.hour_passes()
+
+class Baby(object):
+    def __init__(self,name):
+        self.cycle=0
+        self.name=name
+        print('Hello baby {}!'.format(name))
+    def feed_baby(self):
+        self.cycle=0
+        print('Thank you for feeding baby {}.'.format(self.name))
+    def hour_passes(self):
+        if self.cycle==0:
+            print('Baby {} is sleeping.'.format(self.name))
+        elif self.cycle==1:
+            print('Baby {} is awake. Time for food.'.format(self.name))
+        else:
+            print('Baby {} is CRYING uncontrollably! Feed the Baby!'.format(self.name))
+        self.cycle=self.cycle+1
+
+
+
+
+
+
+
 
 
 # ----------------------------------------------------------------------
